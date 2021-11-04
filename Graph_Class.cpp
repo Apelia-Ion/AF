e4f59683574af0b04d9d26908cc8cd5b6f0e2f3f
Your Name <you@example.com>
2021-11-04 22:48:22 +0200

    first commit

---
+++ Graph_Class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#define MAX 100001
+
+class Graph 
+{
+int nrV;    //number of vertiges
+int nrE;    //number of edges
+bool oriented; // True if the graph is oriented
+
+};
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+return 0;
+}
